fe0b59ec1acc884655aee3cfd3c2962fa92b01f1
AnouarJebri <100767519+AnouarJebri@users.noreply.github.com>
2022-04-12 00:41:40 +0100

    Update minimap.c

--- Anouar/minimap.c
+++ Anouar/minimap.c
@@ -180,3 +180,39 @@ void mouvement(Personne *p ,Personne *pM ,Personne *pMprochaine, int distance, i
 
     }
 }
+/*void initialiserScore(  Score *score )
+{
+	char string[20];
+	SDL_Color couleurNoire ={255,255,255};
+	
+	score->posScore.x=300 ;
+	score->posScore.y=20;
+	score->posFond.x=score->posScore.x-20;
+	score->posFond.y=score->posScore.y-20;	
+
+	score->fondScore = IMG_Load("bois.png");
+	(score->police) = TTF_OpenFont("Colleged.ttf", 20);
+
+	score->scoreActuel=0;
+	sprintf(string, "score = %d ", score->scoreActuel);
+
+	(score->texteScore)=TTF_RenderText_Blended(score->police ,string, couleurNoire);		
+}
+
+
+void afficherScore(SDL_Surface *ecran , Score *score, background bg,int collision )
+{
+
+		
+            SDL_Color couleurNoire ={255,255,255};
+	
+	char string[20];
+	
+	score->scoreActuel=bg.position.x*0.1+50*collision;
+	sprintf(string, "score = %d ", score->scoreActuel);
+	
+	score->texteScore =TTF_RenderText_Blended(score->police,string, couleurNoire);
+	
+	SDL_BlitSurface(score->fondScore, NULL, ecran, &score->posFond);
+	SDL_BlitSurface(score->texteScore, NULL, ecran, &score->posScore);
+}*/
