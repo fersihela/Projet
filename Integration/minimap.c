18f1f6a02ccaab6082a83a35af1d06c6f183eb7f
AnouarJebri <anouar.jebri@esprit.tn>
2022-04-19 01:24:41 -0700

    update

--- Integration/minimap.c
+++ Integration/minimap.c
@@ -9,7 +9,7 @@ void initmap( minimap *m)
 	m->position_mini.x=100;
 	m->position_mini.y=0;
 	m->sprite=NULL;
-    m->sprite=IMG_Load("minimap1.png");
+    m->sprite=IMG_Load("RES/minimap1.png");
 }
 
 void afficherminimap (minimap m, SDL_Surface * screen)
@@ -29,7 +29,7 @@ void initialiser_temps(temps *t)
  t->position.x=1400;
    t->position.y=30;
      t->police = NULL;
-  t->police = TTF_OpenFont("avocado.ttf", 40);
+  t->police = TTF_OpenFont("Chalkboy.ttf", 40);
     strcpy( t->entree,"");
 (t->secondesEcoulees)=0;
 time(&(t->t1));	 //temps du debut
