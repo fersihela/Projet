520262f1885c653fc7f35f946b923bc6084c5124
AnouarJebri <100767519+AnouarJebri@users.noreply.github.com>
2022-05-03 17:43:28 +0100

    Update minimap.c

--- Integration/minimap.c
+++ Integration/minimap.c
@@ -69,7 +69,7 @@ void free_temps(temps *t,SDL_Surface *screen)
 
 
 
-SDL_Color GetPixel(SDL_Surface *Background, int x, int y)
+/*SDL_Color GetPixel(SDL_Surface *Background, int x, int y)
 {
         
         SDL_Color color;
@@ -87,10 +87,10 @@ SDL_Color GetPixel(SDL_Surface *Background, int x, int y)
                 SDL_GetRGB(col, Background->format, &color.r, &color.g, &color.b);
 
                 return (color);
-}
+}*/
 
-
-int collisionPP( Personne p, SDL_Surface * Masque)
+//faite pour verifier le mouvement du personnage dans la minimap
+/*int collisionPP( Personne p, SDL_Surface * Masque)
 {
     SDL_Color test ,couleurnoir= {0,0,0};
 
@@ -126,20 +126,10 @@ int collisionPP( Personne p, SDL_Surface * Masque)
      return collision;
 
 }
+*/
 
-void update_1_(SDL_Rect *prochain , SDL_Rect *principal, SDL_Rect *Pminimap, int distance, int longueur,int longueurM)
-{
-    principal->x=prochain->x;
-    Pminimap->x+=distance*longueur/longueurM;
-}
 
-void update_2_(SDL_Rect *prochain , SDL_Rect *principal, SDL_Rect *Pminimap, int distance, int longueur,int longueurM)
-{
-    principal->x=prochain->x;
-    Pminimap->x-=distance*longueur/longueurM;
-}
-
-void mouvement(Personne *p ,Personne *pM ,Personne *pMprochaine, int distance, int longueur,int longueurM , SDL_Surface *masked , int sens)
+/*void mouvement(Personne *p ,Personne *pM ,Personne *pMprochaine, int distance, int longueur,int longueurM , SDL_Surface *masked , int sens)
 {
     if(sens==1)
     {
@@ -169,7 +159,7 @@ void mouvement(Personne *p ,Personne *pM ,Personne *pMprochaine, int distance, i
                        pM->position_perso.x=0;
                        p->position_perso.x=95;
                     }
-                    else if(collisionPP( *pMprochaine, masked)==0 )
+                    else if(collisionPP( *pMprochaine, p)==0 )
                     {
                         pM->position_perso.x=pMprochaine->position_perso.x;
                         p->position_perso.x-=distance*longueur/longueurM;
@@ -179,40 +169,18 @@ void mouvement(Personne *p ,Personne *pM ,Personne *pMprochaine, int distance, i
                     }
 
     }
-}
-/*void initialiserScore(  Score *score )
-{
-	char string[20];
-	SDL_Color couleurNoire ={255,255,255};
-	
-	score->posScore.x=300 ;
-	score->posScore.y=20;
-	score->posFond.x=score->posScore.x-20;
-	score->posFond.y=score->posScore.y-20;	
-
-	score->fondScore = IMG_Load("bois.png");
-	(score->police) = TTF_OpenFont("Colleged.ttf", 20);
-
-	score->scoreActuel=0;
-	sprintf(string, "score = %d ", score->scoreActuel);
-
-	(score->texteScore)=TTF_RenderText_Blended(score->police ,string, couleurNoire);		
-}
-
-
-void afficherScore(SDL_Surface *ecran , Score *score, background bg,int collision )
-{
-
-		
-            SDL_Color couleurNoire ={255,255,255};
-	
-	char string[20];
-	
-	score->scoreActuel=bg.position.x*0.1+50*collision;
-	sprintf(string, "score = %d ", score->scoreActuel);
-	
-	score->texteScore =TTF_RenderText_Blended(score->police,string, couleurNoire);
-	
-	SDL_BlitSurface(score->fondScore, NULL, ecran, &score->posFond);
-	SDL_BlitSurface(score->texteScore, NULL, ecran, &score->posScore);
 }*/
+void MAJMinimap(SDL_Rect posJoueur,  Personne * pM, SDL_Rect camera, int redimensionnement,int *a,int*b)
+{
+  SDL_Rect posJoueurABS;
+  posJoueurABS.x = 0;
+  posJoueurABS.y = 0;
+  posJoueurABS.x = posJoueur.x + camera.x;
+  posJoueurABS.y = posJoueur.y + camera.y;
+  pM->position_perso.x=(posJoueurABS.x * redimensionnement/100);
+  pM->position_perso.y=(posJoueurABS.y * redimensionnement/100)+21;
+  (*a)=95-pM->position_perso.x;
+  (*b)=87-pM->position_perso.y;
+  pM->position_perso.x+=(*a);
+  pM->position_perso.y+=(*b);
+}
